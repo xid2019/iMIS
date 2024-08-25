26a8bac274dd8900d39fa266229627b0ab7003b2
xid2019 <xid2019@tamu.edu>
2024-08-25 11:22:30 -0500

    connect to postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,16 +75,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',       # Name of the database you created
+        'USER': 'postgres',           # Your PostgreSQL username
+        'PASSWORD': 'mypassword',   # Your PostgreSQL user's password
+        'HOST': 'localhost',        # Set to 'localhost' if running locally
+        'PORT': '5432',             # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
